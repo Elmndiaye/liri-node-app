6072c9e6af7aef18d666318b62f41e5b64b7b90c
Elmalick Ndiaye <elmndiaye@gmail.com>
2018-05-21 16:05:11 -0700

    hw

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 var request = require("request");
+var fs = require('file-system');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
@@ -10,34 +11,84 @@ var action = process.argv[2];
 var value = process.argv[3];
 
 switch (action) {
-case "spotify-this":
-spotifyThis();
+case "song":
+song();
 break;
-case "st":
-spotifyThis();
+case "movie-this":
+movies();
 break;
-case "movie":
-movie();
+case "do-what-it-says":
+does();
 break;
-}
+};
 
-function movie() {
+function movies() {
 var movieName = value;
-// Then run a request to the OMDB API with the movie specified
+
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+// console.log(queryUrl);
 
 request(queryUrl, function(error, response, body) {
 
-// If the request is successful
 if (!error && response.statusCode === 200) {
 
-// Parse the body of the site and recover just the imdbRating
-// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
 console.log("Title is: " + JSON.parse(body).Title);
 console.log("Release Year: " + JSON.parse(body).Year);
+console.log("imdbRating: " + JSON.parse(body).Ratings[0].Value);
+console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+console.log("Plot of the movie: " + JSON.parse(body).Plot);
+console.log("Actors in the movie: " + JSON.parse(body).Actors);
+
 }
 });
+}
+
+function song() {
+
+    spotify.search({ type: 'track', query: value }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+    //   console.log(data.tracks.items[0]); 
+
+      console.log("\nYAY!! You searched for a song! We love music!!" + 
+                  "\nThe artist's name is " + data.tracks.items[0].artists[0].name +
+                  "\nThe songs's name is " + data.tracks.items[0].name +
+                  "\nSee the album at " + data.tracks.items[0].album.external_urls.spotify +
+                  "\nThe Album's name is " + data.tracks.items[0].album.name);
+      });
+
+}
+
+function tweetz(){
+    var params = {screen_name: 'the_gripster'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        // console.log(tweets[0]);
+          for(var i = 0; i < 20; i++) {
+            console.log(tweets[i].text);
+            console.log("Tweeted at: " + tweets[i].created_at);
+            console.log("-----------------");
+          }
+        
+      }
+    });    
+}
+function does (){
+    fs.readFile("do-what-it-says", "utf8", function(error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+
+        var dataArr = data.split(",");
+    
+        console.log(dataArr);
+    
+    });
+
 }
